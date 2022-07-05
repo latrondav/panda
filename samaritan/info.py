193fc25770ec830fabc469602accd686205dcf92
latrondav <latrondav@gmail.com>
2022-07-05 20:00:05 +0300

    commit

--- samaritan/info.py
+++ samaritan/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'pandalogin0@gmail.com'
-EMAIL_HOST_PASSWORD = 'Panda12345'
+EMAIL_HOST_PASSWORD = 'rbbbgdhygmqsiupr'
 EMAIL_PORT = 587
