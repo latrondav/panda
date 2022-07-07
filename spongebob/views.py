e2865bcf004644d2228d3ade434e41f476fa37b8
latrondav <latrondav@gmail.com>
2022-07-07 11:36:53 +0300

    django update

--- spongebob/views.py
+++ spongebob/views.py
@@ -12,7 +12,7 @@ from django.core.mail import send_mail
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
-from django.utils.encoding import force_bytes, force_text
+from django.utils.encoding import force_bytes, force_str
 from .models import contact
 from spongebob.models import contact
 from . token import generate_token
@@ -127,7 +127,7 @@ def signout_page(request):
 
 def activate(request, uidb64, token):
     try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
+        uid = force_str(urlsafe_base64_decode(uidb64))
         myuser = User.objects.get(pk=uid)
     except (TypeError, ValueError, OverflowError, User.DoesNotExist):
         myuser = None
