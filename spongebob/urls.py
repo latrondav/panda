6c78208928f8fe9ace44c0a676892518ab3a2647
latrondav <latrondav@gmail.com>
2022-07-12 20:36:49 +0300

    password reset updated

--- spongebob/urls.py
+++ spongebob/urls.py
@@ -19,10 +19,10 @@ urlpatterns = [
     path('team/', views.team_page),
 
     #reset password path
-    path('resetpasswordform/', auth_views.PasswordResetView.as_view(template_name="password_reset_form.html"), name= "password_reset_form"),
-    path('resetpassworddone/', auth_views.PasswordResetDoneView.as_view(template_name="password_reset_done.html"), name= "password_reset_done"),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"), name= "password_reset_confirm"),
-    path('resetpasswordcomplete/', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"), name= "password_reset_complete"),
+    path('resetpasswordform/', auth_views.PasswordResetView.as_view(template_name="registration/pw_reset_form.html"), name= "password_reset_form"),
+    path('resetpassworddone/', auth_views.PasswordResetDoneView.as_view(template_name="registration/pw_reset_done.html"), name= "password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="registration/pw_reset_confirm.html"), name= "password_reset_confirm"),
+    path('resetpasswordcomplete/', auth_views.PasswordResetCompleteView.as_view(template_name="registration/pw_reset_complete.html"), name= "password_reset_complete"),
 
     #change password path
     path('changepasswordform/', auth_views.PasswordChangeView.as_view(), name="password_change_form"),
