251f072c6746e878f1e384baa0736513278b03fb
latrondav <latrondav@gmail.com>
2022-09-21 17:21:13 +0300

    team update

--- spongebob/views.py
+++ spongebob/views.py
@@ -13,8 +13,7 @@ from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.utils.encoding import force_bytes, force_str
-from .models import contact
-from spongebob.models import contact
+from . models import contact
 from . token import generate_token
 from django.core.mail import EmailMessage, send_mail
 
