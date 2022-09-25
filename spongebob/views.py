0f6d47c44fb10d64fafd2eba88725293d3527222
latrondav <latrondav@gmail.com>
2022-09-25 11:09:18 +0300

    book bus assured

--- spongebob/views.py
+++ spongebob/views.py
@@ -13,7 +13,7 @@ from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.utils.encoding import force_bytes, force_str
-from . models import Contact, Bus
+from . models import Contact, Bus, BookedSeats, BusBooking
 from . token import generate_token
 from .forms import BookBusForm
 from django.core.mail import EmailMessage, send_mail
@@ -210,3 +210,111 @@ def bus_book(request, *args, **kwargs):
             #return render(request, 'book_bus.html', {'bus':bus_creation, 'busId':busId, 'seats':new_dict})
     
     return render(request, 'book_bus.html')
+
+def book_bus(request,*args,**kwargs):
+    if request.method =='POST':
+        # getting form input names
+        busId=request.POST["bus_id"]
+        bus_creation=BookBusForm()
+        
+        bus_name_id=Bus.objects.get(id=busId)
+        bus_seats=bus_name_id.no_of_seats
+        busSeats=int(bus_seats)
+        new_dict={val:0 for val in range(busSeats)}
+
+        # if any seats were booked 
+        if Bus.objects.filter(id=busId).exists():
+            bus=Bus.objects.filter(id=busId).first()
+            temp =  bus.BookedSeats.seats
+            seats=temp.split(',')
+            seats.pop()
+            i=0
+            for i in range(len(seats)):
+                    seat=seats[i]
+                    temp=int(seat)
+                    new_dict[temp]=1
+            return render(request,'buses/book_bus.html',{'bus':bus_creation, 'busId':busId, 'seats':new_dict})
+        else:
+            return render(request,'buses/book_bus.html',{'bus':bus_creation, 'busId':busId, 'seats':new_dict})
+   
+
+def booking_details(request,*args, **kwargs):
+    if request.method == 'POST':
+        bus_id=request.POST.get('bus_id')
+        seat_nos=request.POST.get('seat_nos')
+        seat_nos=seat_nos[:-1]
+        no_of_seats=int(request.POST.get('count'))
+        phone_no=request.POST.get('phone_number')
+        picking_station=request.POST.get('pick_up_station')
+        
+        bus=Bus.objects.get(id=bus_id)
+        if bus:
+            if bus.remaining_seats > no_of_seats or bus.remaining_seats == int(no_of_seats) :
+                username=request.user.username
+                user_id=request.user.id
+                email=request.user.email
+                name=bus.name
+                price=bus.price
+                total_price=int(no_of_seats) * bus.price
+                source=bus.source
+                destination=bus.destination
+                date=bus.date
+                seats= seat_nos
+                time=bus.departure_time
+                bus_remaining_seats=bus.remaining_seats - int(no_of_seats)
+               
+                booked_seats=bus.bookedseats.seats
+                booked_seats += ',' + seats
+                BookedSeats.objects.update(seats=booked_seats, bus=bus)
+
+
+
+                # try:
+                #     booked_seats, created=BookedSeats.objects.get_or_create(id=bus_id)
+                    
+                #     if created:
+                #         booked_seats.seats += seats
+                #         booked_seats.save()
+                #         print(booked_seats)
+                # except:
+                #         bus=Bus.objects.filter(id=bus_id).first()
+                #         booked_seats=BookedSeats(seats=seat_nos, bus=bus)
+                #         booked_seats.save()
+                
+             
+                Bus.objects.filter(id=bus_id).update(remaining_seats=bus_remaining_seats)
+                booking_details=BusBooking.objects.create(
+                   user_name=username,
+                    user_email=email,
+                    user_id=user_id,
+                    source=source,
+                    destination=destination,
+                    bus_name=name,
+                    date=date,
+                    price=total_price,
+                    bus_id=bus_id,
+                    time=time,
+                    no_of_seats=no_of_seats,
+                    seat_numbers=seat_nos,
+                    phone_no=phone_no,
+                    pick_up_station=picking_station
+                )
+                context={
+                    'booking_details':booking_details,
+                    'seat_price':price,
+                    'bus':bus
+                }
+                print('============Booking ID==========', booking_details.id)     
+                return render(request,'buses/booking_details.html',locals())
+            else:
+                context={
+                    'error':f'Input {bus.remaining_seats} seat/s or less.'
+                }    
+                return render(request,'buses/book_bus.html', context)
+        else:
+            context={
+              'error':'Please input a valid Bus ID'
+            }    
+            return render(request,'buses/book_bus.html', context)        
+    else:
+        return render(request,'buses/book_bus.html')
