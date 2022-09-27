b9b3ffa87a3b80066bd5db4043009728bd7317d9
latrondav <89303631+latrondav@users.noreply.github.com>
2022-09-27 15:26:16 +0300

    db config

--- samaritan/settings.py
+++ samaritan/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'samaritan.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'pandadb.sqlite3',
     }
 }
 
