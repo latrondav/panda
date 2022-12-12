591451f1ea3eb6f01f7c28372c1a56d2d270acbc
latrondav <89303631+latrondav@users.noreply.github.com>
2022-12-12 18:19:03 +0300

    progress

--- spongebob/views.py
+++ spongebob/views.py
@@ -17,7 +17,7 @@ from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.utils.encoding import force_bytes, force_str
 from . models import Contact, Bus, BookedSeats, BusBooking
-from . token import generate_token
+from .token_1 import generate_token
 from .forms import BookBusForm
 from django.core.mail import EmailMessage, send_mail
 
