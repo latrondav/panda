26ad645e42e6c605f21865cef55927f51baf52df
latrondav <89303631+latrondav@users.noreply.github.com>
2023-01-03 16:20:59 +0300

    bugs fixed

--- samaritan/settings.py
+++ samaritan/settings.py
@@ -18,6 +18,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+EMAIL_BACKEND = EMAIL_BACKEND
 EMAIL_USE_TLS = EMAIL_USE_TLS
 EMAIL_HOST = EMAIL_HOST
 EMAIL_HOST_USER = EMAIL_HOST_USER
