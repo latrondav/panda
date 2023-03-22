94d1566deb7c779a35624d221edff0210d37d5a7
latrondav <89303631+latrondav@users.noreply.github.com>
2023-03-22 11:43:33 +0300

    Update info.py

--- samaritan/info.py
+++ samaritan/info.py
@@ -2,5 +2,5 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'panbusadm@gmail.com'
-EMAIL_HOST_PASSWORD = 'kyydbkgvidhvryee'
+EMAIL_HOST_PASSWORD = 'njflhsxaqjodokjh'
 EMAIL_PORT = 587
