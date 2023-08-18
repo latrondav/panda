8bb4eba58a0a48a5e4ed5960b1b5f279c1addedf
LATIM RONNIE DAVID <89303631+latrondav@users.noreply.github.com>
2023-08-18 14:44:57 +0300

    progress

--- samaritan/settings.py
+++ samaritan/settings.py
@@ -14,6 +14,8 @@ from operator import truediv
 from pathlib import Path
 from .info import *
 import os
+from django.core.management.utils import get_random_secret_key
+from urllib.parse import urlparse
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,13 +31,12 @@ EMAIL_PORT = EMAIL_PORT
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-=)^tm-yzp7l7l)mk&pyo%=-8(8(!1mtv!4a7%^-qu!#5kv_esu')
+SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
-
 
 # Application definition
 
@@ -86,12 +87,26 @@ WSGI_APPLICATION = 'samaritan.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'pandadb.sqlite3',
+if os.getenv('DATABASE_URL', '') != '':
+    r = urlparse(os.environ.get('DATABASE_URL'))
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.path.relpath(r.path, '/'),
+            'USER': r.username,
+            'PASSWORD': r.password,
+            'HOST': r.hostname,
+            'PORT': r.port,
+            'OPTIONS': {'sslmode': 'require'},
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'pandadb.sqlite3',
+        }
+    }
 
 
 # Password validation
