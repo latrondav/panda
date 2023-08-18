db0c11f26ffd35a5d97a5c4c947be755137b5ec3
LATIM RONNIE DAVID <89303631+latrondav@users.noreply.github.com>
2023-08-18 13:50:26 +0300

    Update settings.py

--- samaritan/settings.py
+++ samaritan/settings.py
@@ -29,10 +29,10 @@ EMAIL_PORT = EMAIL_PORT
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=)^tm-yzp7l7l)mk&pyo%=-8(8(!1mtv!4a7%^-qu!#5kv_esu'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-=)^tm-yzp7l7l)mk&pyo%=-8(8(!1mtv!4a7%^-qu!#5kv_esu')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
