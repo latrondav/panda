24e83492da0448c69133ced21eb8a21258b88df0
LATIM RONNIE DAVID <89303631+latrondav@users.noreply.github.com>
2023-08-18 14:28:55 +0300

    Update settings.py

--- samaritan/settings.py
+++ samaritan/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-=)^tm-yzp7l7l)mk&pyo%=-8(8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 
 # Application definition
