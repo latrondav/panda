dcfa6fa31d744340b581b536a6dbca332060d99b
LATIM RONNIE DAVID <89303631+latrondav@users.noreply.github.com>
2023-08-18 14:19:43 +0300

    Update settings.py

--- samaritan/settings.py
+++ samaritan/settings.py
@@ -130,11 +130,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_ROOT = BASE_DIR/'deploymentstaticfiles'
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
